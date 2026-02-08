2eaa29569f8f9798d45a1e7f05b9417d799f595c
@elbert1104 <124583187+elbert1104@users.noreply.github.com>
2026-02-08 18:17:32 +0800

    Added Additional Feature

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -1,8 +1,90 @@
-import React from 'react'
+import React, {useMemo, useState} from 'react'
+import {ListView} from "@/components/refine-ui/views/list-view.tsx";
+import {Breadcrumb} from "@/components/refine-ui/layout/breadcrumb.tsx";
+import {Search} from "lucide-react";
+import {Input} from "@/components/ui/input.tsx";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select.tsx";
+import {DEPARTMENT_OPTIONS} from "@/constants";
+import {CreateButton} from "@/components/refine-ui/buttons/create.tsx";
+import {DataTable} from "@/components/refine-ui/data-table/data-table.tsx";
+import {useTable} from "@refinedev/react-table";
+import {Subject} from "@/types";
+
 
 const SubjectsList = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [selectedDepartment, setSelectedDepartment] = useState('all')
+
+    const subjectTable = useTable<Subject>({
+        columns: useMemo<ColumnDef<Subject>[]>(() => [
+            {
+                id: 'code',
+                accessorKey: 'code',
+                size: 100,
+                header: () => <p className="column-title ml-2">Code</p>,
+                cell: ({ getValue}) => <Badge>{getValue<string>()}</Badge>
+            },
+
+], []),
+    refineCoreProps: {
+        resource: 'subjects',
+            pagination: { pageSize: 10, mode: 'server'},
+        filters: {},
+        sorters: {},
+    }
+
+});
+
     return (
-        <div>List</div>
+        <ListView>
+            <Breadcrumb/>
+
+            <h1 className="page-title">Subjects</h1>
+
+            <div className="intro-row">
+                <p>Quick access to essential metrics and management tools.</p>
+
+                <div className="actions-row">
+                    <div className="search-field">
+                        <Search className="search-icon"/>
+
+                        <Input
+                            type="text"
+                            placeholder="Search by name ..."
+                            className="pl-10 w-full"
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex gap-2 w-full sm:w-auto">
+                        <Select value={selectedDepartment} onValueChange={setSelectedDepartment}>
+                            <SelectTrigger>
+                                <SelectValue placeholder="Filter by department"/>
+                            </SelectTrigger>
+
+                            <SelectContent>
+                                <SelectItem value="all">
+                                    All Departments
+                                </SelectItem>
+                                {DEPARTMENT_OPTIONS.map(department => (
+                                    <SelectItem key={department.value} value={department.value}>
+                                        {department.label}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+
+                        <CreateButton/>
+                    </div>
+                </div>
+            </div>
+
+            <DataTable table={subjectTable}/>
+        </ListView>
     )
 }
+
+
+import {Badge} from "@/components/ui/badge.tsx";
+import {ColumnDef} from "@tanstack/react-table";
 export default SubjectsList
